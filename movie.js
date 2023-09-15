0cd85e633b204aa3fe4a784f13102154655434bc
Godwin Chinenye Godlove <chinenye.godlove@gmail.com>
2023-09-15 12:13:20 +0100

    first commit of movie search app

---
+++ movie.js
@@ -0,0 +1,51 @@
+// https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=66b303b4c38e3570cc295ebc9a44bb24&page=1
+
+const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=66b303b4c38e3570cc295ebc9a44bb24&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=66b303b4c38e3570cc295ebc9a44bb24&query=";
+
+const form = document.querySelector("#form");
+const search = document.querySelector("#query");
+const cardSection = document.querySelector(".row");
+const notif = document.querySelector(".notifications");
+
+returnMovies(APILINK);
+
+async function returnMovies(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        notif.innerHTML = "";
+        console.log(data)
+        const displayCards = data.results.map(element => {
+            if (element.backdrop_path) {
+                return `<div class="card">
+                            <div class="card-img"><img src="${IMG_PATH + element.poster_path}" alt="poster-image" class="thumbnail"></div>
+                                <div class="movie-desc">
+                                        <div>Title: ${element.title.length > 25 ? element.title.slice(0, 23) + "..." : element.title}</div>
+                                        <div>Rating: ⭐${element.vote_average}</div>
+                                    <div class="desc-2">Release Date: ${element.release_date}</div>
+                                </div>
+                        </div> 
+                        `;
+            }
+        }).join("")
+
+        cardSection.innerHTML = displayCards;
+
+        if (!data.results.length > 0) {
+            notif.innerHTML = "No search results...";
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+form.addEventListener("submit", (searchEvent) => {
+    searchEvent.preventDefault();
+    cardSection.innerHTML = "";
+    const searchString = search.value;
+    returnMovies(SEARCHAPI + searchString);
+})
+
