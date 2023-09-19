bfda7ef1e5239a753d4772e887dab8d5d716d80b
Godwin Chinenye Godlove <chinenye.godlove@gmail.com>
2023-09-19 09:24:53 +0100

    changed css features

--- movie.js
+++ movie.js
@@ -16,7 +16,6 @@ async function returnMovies(url) {
         const response = await fetch(url);
         const data = await response.json();
         notif.innerHTML = "";
-        console.log(data)
         const displayCards = data.results.map(element => {
             if (element.backdrop_path) {
                 return `<div data-id="${element.id}" class="card">
@@ -48,33 +47,47 @@ async function returnMovies(url) {
 function showMovie(card) {
     card.addEventListener("click", async function () {
         popup.style.display = "block";
-        let idNo = card.getAttribute("data-id")
+        let idNo = card.getAttribute("data-id");
+
         try {
             const response = await fetch(`https://api.themoviedb.org/3/movie/${idNo}?api_key=66b303b4c38e3570cc295ebc9a44bb24&append_to_response=videos`);
             const data = await response.json();
-            console.log(data)
 
             popup.innerHTML = `
         <div class="popup">
             <div class="popup-img"><img src="${IMG_PATH + data.poster_path}" alt="poster-img"></div>
+
             <div class="popup-details">
                 <div class="popup-title"><h1>${data.original_title}</h1><i>Tagline: ${data.tagline}</i></div>
+
                 <div class="popup-item"><h4>Genres:</h4> ${data.genres.map(genre => genre.name).join(" | ")}</div>
+
                 <div class="popup-website"><h4>Website:</h4> ${data.homepage}</div>
+
                 <div class="popup-date"><h4>Release Date: ${data.release_date}</h4><span><h4>Rating: </h4>⭐${data.vote_average}</span></div>
+
                 <div class="popup-item"><h4>Budget:</h4> $ ${data.budget} USD</div>
+
                 <div class="popup-item"><h4>Revenue:</h4> $ ${data.revenue} USD</div>
+
                 <div class="popup-description">${data.overview}</div>
+
                 <div class="popup-bottom">
                     <div class="movie-companies">
                     <h4>Production Companies:</h4>
                     ${data.production_companies.map(el => {
-                return `<span>${el.name}</span>`
-            }).join("|")}
+                return `<span>* ${el.name}</span>`
+            }).join("")}
                     </div>
-                    
+                    <div class="video">
+                    <h4>Watch Trailer</h4>
+                    <iframe id="ytplayer"  type="video" 
+                        src="https://www.youtube.com/embed/${data.videos.results[0].key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                    </iframe></div> 
                 </div>
+
             </div>
+
         </div>
         `
         } catch (err) {
@@ -83,13 +96,10 @@ function showMovie(card) {
     })
 }
 
+
 popup.addEventListener("click", () => {
     popup.style.display = "none";
 })
-
-function removeTrailingSlash(str) {
-    return str.replace(/\/+$/, '');
-}
 
 form.addEventListener("submit", (searchEvent) => {
     searchEvent.preventDefault();
@@ -97,10 +107,3 @@ form.addEventListener("submit", (searchEvent) => {
     const searchString = search.value;
     returnMovies(SEARCHAPI + searchString);
 })
-
-{/* <img src=${LOGO_PATH + el.logo_path}/> */ }
-
-{/* <iframe id="ytplayer" class="video" type="video" width="360" height="220"
-                        src="https://www.youtube.com/embed/${data.videos.results[0].key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
-                        frameborder="0">
-                    </iframe> */}
