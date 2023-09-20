50f11ee5f18a3b7231d95ffea4214a7084abef21
Godwin Chinenye Godlove <chinenye.godlove@gmail.com>
2023-09-20 09:20:17 +0100

    added loading, error feedback features and modified close modal features

--- movie.js
+++ movie.js
@@ -16,6 +16,7 @@ async function returnMovies(url) {
         const response = await fetch(url);
         const data = await response.json();
         notif.innerHTML = "";
+
         const displayCards = data.results.map(element => {
             if (element.backdrop_path) {
                 return `<div data-id="${element.id}" class="card">
@@ -38,6 +39,7 @@ async function returnMovies(url) {
     }
     catch (err) {
         console.log("MOVIE_FETCH_ERROR", err);
+        notif.innerHTML = "We've encountered an error fetching your movies, try again!"
     }
     const allCards = document.querySelectorAll(".card");
     allCards.forEach(card => showMovie(card));
@@ -54,54 +56,55 @@ function showMovie(card) {
             const data = await response.json();
 
             popup.innerHTML = `
-        <div class="popup">
-            <div class="popup-img"><img src="${IMG_PATH + data.poster_path}" alt="poster-img"></div>
+            <div data-clear="popup" class="popup">
+                <div class="close">&times;</div>
+                <div class="popup-img"><img src="${IMG_PATH + data.poster_path}" alt="poster-img"></div>
 
-            <div class="popup-details">
-                <div class="popup-title"><h1>${data.original_title}</h1><i>Tagline: ${data.tagline}</i></div>
+                <div class="popup-details">
+                    <div class="popup-title"><h1>${data.original_title}</h1><i>Tagline: ${data.tagline}</i></div>
 
-                <div class="popup-item"><h4>Genres:</h4> ${data.genres.map(genre => genre.name).join(" | ")}</div>
+                    <div class="popup-item"><h4>Genres:</h4> ${data.genres.map(genre => genre.name).join(" | ")}</div>
 
-                <div class="popup-website"><h4>Website:</h4> ${data.homepage}</div>
+                    <div class="popup-website"><h4>Website:</h4> ${data.homepage}</div>
 
-                <div class="popup-date"><h4>Release Date: ${data.release_date}</h4><span><h4>Rating: </h4>⭐${data.vote_average}</span></div>
+                    <div class="popup-date"><h4>Release Date: ${data.release_date}</h4><span><h4>Rating: </h4>⭐${data.vote_average}</span></div>
 
-                <div class="popup-item"><h4>Budget:</h4> $ ${data.budget} USD</div>
+                    <div class="popup-item"><h4>Budget:</h4> $ ${data.budget} USD</div>
 
-                <div class="popup-item"><h4>Revenue:</h4> $ ${data.revenue} USD</div>
+                    <div class="popup-item"><h4>Revenue:</h4> $ ${data.revenue} USD</div>
 
-                <div class="popup-description">${data.overview}</div>
+                    <div class="popup-description">${data.overview}</div>
 
-                <div class="popup-bottom">
-                    <div class="movie-companies">
-                    <h4>Production Companies:</h4>
-                    ${data.production_companies.map(el => {
+                    <div class="popup-bottom">
+                        <div class="movie-companies">
+                        <h4>Production Companies:</h4>
+                            ${data.production_companies.map(el => {
                 return `<span>* ${el.name}</span>`
             }).join("")}
+                        </div>
+                        <div class="video">
+                        <h4>Watch Trailer</h4>
+                        <iframe id="ytplayer"  type="video" 
+                            src="https://www.youtube.com/embed/${data.videos.results[0].key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                        </iframe></div> 
                     </div>
-                    <div class="video">
-                    <h4>Watch Trailer</h4>
-                    <iframe id="ytplayer"  type="video" 
-                        src="https://www.youtube.com/embed/${data.videos.results[0].key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                    </iframe></div> 
+
                 </div>
 
-            </div>
-
-        </div>
+                </div>
         `
         } catch (err) {
             console.log("SHOW_MOVIE_ERROR", err);
         }
+        const closeModal = popup.children[0].children[0];
+        closeModal.addEventListener("click", () => {
+            popup.style.display = "none";
+        })
     })
 }
 
-
-popup.addEventListener("click", () => {
-    popup.style.display = "none";
-})
-
 form.addEventListener("submit", (searchEvent) => {
+    notif.innerHTML = "Loading..."
     searchEvent.preventDefault();
     cardSection.innerHTML = "";
     const searchString = search.value;
